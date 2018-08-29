0a05d4775789ac5f16f3e3227b373f3db411661d
geohoperepresents@gmail.com <geohoperepresents@gmail.com>
2018-08-29 13:37:20 +0530

    add

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ def question():
 	
 @app.route("/answer")
 def question():
-    return render_template("answer.html")
+    return render_template("question.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
